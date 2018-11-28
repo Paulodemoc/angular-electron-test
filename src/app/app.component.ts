ab29ab2f516f1bb00fe7d8b4ff2a7c6896a9c77f
Paulo <paulodemoc@gmail.com>
2018-11-28 07:55:59 -0200

    First Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs/Observable/interval';
+import 'rxjs/add/observable/interval';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/takeWhile';
+import 'rxjs/add/operator/do';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-electron';
+
+  max = 1;
+  current = 0;
+
+  start(){
+    interval(100)
+      .takeWhile(_ => !this.isFinished)
+      .do(i => this.current += 0.1)
+      .subscribe();
+  }
+
+  finish(){
+    this.current = this.max;
+  }
+
+  reset(){
+    this.current = 0;
+  }
+
+  get maxVal(){
+    return isNaN(this.max) || this.max < 0.1 ? 0.1 : this.max;
+  }
+
+  get currentVal(){
+    return isNaN(this.current) || this.current < 0 ? 0 : this.current;
+  }
+
+  get isFinished(){
+    return this.currentVal >= this.maxVal;
+  }
+}
